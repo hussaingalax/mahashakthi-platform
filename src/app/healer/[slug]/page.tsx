9183029c74d6ee1bef0db54bee2f7566efe408b9
hussaingalax <hussaingalax@gmail.com>
2026-02-27 11:01:17 +0530

    Remove duplicate 'use client' directive

--- src/app/healer/[slug]/page.tsx
+++ src/app/healer/[slug]/page.tsx
@@ -1,6 +1,7 @@
+"use client"
+
 export const dynamic = "force-dynamic"
 
-"use client"
 import { useEffect, useState } from "react"
 import { useParams } from "next/navigation"
 import { supabase } from "@/lib/supabase"
