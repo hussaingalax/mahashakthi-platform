634be37c06494082ca4ece87ecfc898b380dcc6b
hussaingalax <hussaingalax@gmail.com>
2026-06-07 18:52:50 +0530

    Refactor Supabase client initialization
    Updated Supabase client initialization to use non-null assertions for environment variables.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,8 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ??
-  "https://cwjngbptwcasfjivsofw.supabase.co/rest/v1/"
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
 
-const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3am5nYnB0d2Nhc2ZqaXZzb2Z3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4Mjk0OTcsImV4cCI6MjA5NjQwNTQ5N30.TTzbEaQlC1xOTuZjeRXzgJX7p8-nvoRkoyT2F1MEcIA"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
